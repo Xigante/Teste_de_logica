1faba289a1d719babddd8a19b05658dbb31f8753
Albertasse <125309934+Xigante@users.noreply.github.com>
2023-06-23 11:28:26 -0300

    Resposta em js

---
+++ resposta.js
@@ -0,0 +1,28 @@
+var numeroUm = 1;
+var stringUm = "1";
+var numeroTrinta = 30;
+var stringTrinta = "30";
+var numeroDez = 10;
+var stringDez = "10";
+
+
+if( numeroUm == stringUm){
+    console.log("As variáveis numeroUm e stringUm tem o mesmo valor, mas tipos diferentes")
+}
+else{
+    console.log("As variáveis numeroUm e stringUm não tem o mesmo valor")
+}
+
+
+if(numeroTrinta == stringTrinta){
+    console.log("As variáveis numeroUm e stringUm tem o mesmo valor, mas tipos diferentes")
+}
+else{
+    console.log("As variáveis numeroUm e stringUm não tem o mesmo valor")
+}
+if(numeroDez == stringDez){
+    console.log("As variáveis numeroUm e stringUm tem o mesmo valor, mas tipos diferentes")
+}
+else{
+    console.log("As variáveis numeroUm e stringUm não tem o mesmo valor")
+}
